08fd30b9022ccbb39b8ea8802111f3b01e57a530
tsukemonoGit <mine43try@gmail.com>
2026-06-20 14:30:00 +0900

    fix

--- app/scripts/update-reactions.ts
+++ app/scripts/update-reactions.ts
@@ -1,10 +1,9 @@
 import fs from 'fs';
-import path from 'path';
 
 const filePath = './src/lib/data/reactions.json';
 async function main() {
   try {
-    const raw = fs.readFileSync(path, 'utf-8')
+    const raw = fs.readFileSync(filePath, 'utf-8')
 
     //TODO: update
 
