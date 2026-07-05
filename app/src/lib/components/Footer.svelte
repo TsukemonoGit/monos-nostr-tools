cbbcd3b8a9ab695dfd1abe2e7a52c3bfe74f5826
tsukemonoGit <mine43try@gmail.com>
2026-07-05 19:07:22 +0900

    json edit window

--- app/src/lib/components/Footer.svelte
+++ app/src/lib/components/Footer.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+	import { PencilSparkles } from '@lucide/svelte';
 </script>
 
 <footer class="font-maru h-60">
 	<div class="myContainer">Footer フッター font-maru ₍ ･ᴗ･ ₎</div>
+	{#if import.meta.env.DEV}
+		<a
+			href={'/gui'}
+			class="m-2 flex items-center gap-2 font-bold bg-gray-400 w-fit rounded-full px-4 py-2 button-animation"
+			><PencilSparkles />Edit</a
+		>
+	{/if}
 </footer>
 
 <style lang="postcss">
